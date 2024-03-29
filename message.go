9c9dc539b73c2e7c49774389f70c561ac760bde9
chenbiao <149027310@qq.com>
2024-03-29 10:37:27 +0800

    发送图片消息

--- message.go
+++ message.go
@@ -1,13 +1,33 @@
 package dgfeishu
 
 import (
+	"errors"
 	dgctx "github.com/darwinOrg/go-common/context"
 	dglogger "github.com/darwinOrg/go-logger"
 	"github.com/go-lark/lark"
 )
 
-func SimpleSendTextMessage(ctx *dgctx.DgContext, webhook string, content string) (*lark.PostNotificationV2Resp, error) {
-	dglogger.Infof(ctx, "simple send text message, webhook: %s, content: %s", webhook, content)
+func SimpleSendTextMessage(ctx *dgctx.DgContext, webhook, content string) (*lark.PostNotificationV2Resp, error) {
+	dglogger.Infof(ctx, "simple send text message, content: %s", content)
 	bot := lark.NewNotificationBot(webhook)
 	return bot.PostNotificationV2(lark.NewMsgBuffer(lark.MsgText).Text(content).Build())
+}
+
+func SimpleSendImageMessage(ctx *dgctx.DgContext, appId, appSecret string, imageFile string) (*lark.PostNotificationV2Resp, error) {
+	dglogger.Infof(ctx, "simple send image message, imageFile: %s", imageFile)
+	bot := lark.NewChatBot(appId, appSecret)
+	_, _ = bot.GetTenantAccessTokenInternal(true)
+	//bot := lark.NewNotificationBot(webhook)
+	//bot.SetDomain("https://open.feishu.cn")
+	resp, err := bot.UploadImage(imageFile)
+	if err != nil {
+		dglogger.Errorf(ctx, "upload image[%s] error: %s", imageFile, err)
+		return nil, err
+	}
+	if resp.Code != 0 {
+		dglogger.Errorf(ctx, "upload image[%s] error: %s", imageFile, resp.Msg)
+		return nil, errors.New(resp.Msg)
+	}
+
+	return bot.PostNotificationV2(lark.NewMsgBuffer(lark.MsgImage).Image(resp.Data.ImageKey).Build())
 }
--- message_test.go
+++ message_test.go
@@ -4,12 +4,23 @@ import (
 	dgctx "github.com/darwinOrg/go-common/context"
 	dgfeishu "github.com/darwinOrg/go-feishu"
 	dglogger "github.com/darwinOrg/go-logger"
+	"os"
 	"testing"
 )
 
 func TestSimpleSendTextMessage(t *testing.T) {
 	ctx := &dgctx.DgContext{TraceId: "123"}
-	resp, err := dgfeishu.SimpleSendTextMessage(ctx, "https://open.feishu.cn/open-apis/bot/v2/hook/102301d5-cf25-4269-b296-bf6df737643a", "验证码：000000")
+	resp, err := dgfeishu.SimpleSendTextMessage(ctx, os.Getenv("WEBHOOK"), "验证码：000000")
+	if err != nil {
+		dglogger.Error(ctx, err)
+		return
+	}
+	dglogger.Info(ctx, resp)
+}
+
+func TestSimpleSendImageMessage(t *testing.T) {
+	ctx := &dgctx.DgContext{TraceId: "123"}
+	resp, err := dgfeishu.SimpleSendImageMessage(ctx, os.Getenv("APP_ID"), os.Getenv("APP_SECRET"), "test.jpg")
 	if err != nil {
 		dglogger.Error(ctx, err)
 		return
